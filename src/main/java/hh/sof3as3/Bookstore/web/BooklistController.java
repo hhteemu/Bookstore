f72c8e5241070a4a8d47c969919ebe6d10c9ee9d
Teemu Asikainen <teemu.asikainen@myy.haaga-helia.fi>
2023-02-11 17:25:27 +0200

    Added a listpage

---
+++ src/main/java/hh/sof3as3/Bookstore/web/BooklistController.java
@@ -0,0 +1,20 @@
+package hh.sof3as3.Bookstore.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import hh.sof3as3.Bookstore.domain.BookRepository;
+
+@Controller
+public class BooklistController {
+	@Autowired
+	private BookRepository repository;
+	
+	@GetMapping("/booklist")
+	public String bookList(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist"; //booklist.html
+	}
+}
