2dadf207d7d056b82394c97a003a300a667632a9
Teemu Asikainen <teemu.asikainen@myy.haaga-helia.fi>
2023-02-12 17:46:39 +0200

    Added a delete book and add book functionalities.

--- src/main/java/hh/sof3as3/Bookstore/web/BooklistController.java
+++ src/main/java/hh/sof3as3/Bookstore/web/BooklistController.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import hh.sof3as3.Bookstore.domain.Book;
 import hh.sof3as3.Bookstore.domain.BookRepository;
 
 @Controller
@@ -16,5 +20,26 @@ public class BooklistController {
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist"; //booklist.html
+	}
+	
+	//Deleting a book from a database
+	@GetMapping("delete/{id}")
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
+	}
+	
+	//Adding a book
+	@RequestMapping(value = "/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook"; //addbook.html
+	}
+	
+	//Saving a book
+	@PostMapping("/save")
+	public String saveBook(Book book) {
+		repository.save(book);
+		return "redirect:booklist"; //booklist.html
 	}
 }
